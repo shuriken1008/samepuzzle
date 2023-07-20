f60c55c43242f24f445cc926de3a9d832abb8a7a
ここあ 甘庶 <cocoapowder88@outlook.jp>
2023-07-21 00:47:07 +0900

    server fixed

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+import asyncio
+import errno
 import subprocess
+import sys
 import discord_webhook
 import queue
 import threading
@@ -6,50 +9,45 @@ import time
 
 webhook = discord_webhook.Webhook("https://discord.com/api/webhooks/1131413404599455754/4o4kIdRTzONA2EtKamBz_3Nbpb8eodVy_KmWGAiUP4e3ThytupkFJmJ_oSinhDogXiFj")
 
-q = queue.Queue()
 
-
-
-def execute_bash_command(command):
+async def listner(cmd):
     process = subprocess.Popen(
-        command, 
+        cmd, 
         shell=True,
         stdout=subprocess.PIPE, 
         stderr=subprocess.STDOUT, 
         text=True
     )
-    while True:
-        output = process.stdout.readline()
-        if output == '' and process.poll() is not None:
-            break
-        if output:
-            out = str(output.strip())
-            print(out)
-            out = f"```json\n{out}```"
-            webhook.upload_to_discord(out)
-            
-    return process.poll()
-
-
-
-
-def send_loop():
-    while True:
+    while process.poll() is None:
         try:
-            if q.empty:
+            await asyncio.sleep(0.8)
+            # ノンブロッキングモードなので読める分だけ読む
+            buf = process.stdout.read()
+            if buf == None:
                 continue
-            webhook.upload_to_discord(q.get())
-            time.sleep(0.8)
-            pass
-        except Exception as e:
-            print(e)
+            try:
+                buf = buf.decode()
+            except Exception as e:
+                print(str(e))
+                pass
+            
+            webhook.upload_to_discord(f"```json{buf}```")
+        except IOError as e:
+            # 読むべき内容がない場合はIOError(11,
+            # "Resource temporarily unavailable")が
+            # スローされるので待機
+            if e.errno == errno.EAGAIN:
+                await asyncio.sleep(1)
+        except:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            text = f"エラー:\ntype: {exc_type}\nobj: {exc_obj}\ntb : {exc_tb.tb_lineno}" #type: ignore
+            print(text)
+
 
 
 if __name__ == "__main__":
     command_to_execute = "java samepuzzle/Server" 
     
-    t1 = threading.Thread(target=execute_bash_command, args=(command_to_execute,))
-    t1.start()
-
+    asyncio.run(listner(command_to_execute))
     #t2 = threading.Thread(target=send_loop)
     #t2.start()
