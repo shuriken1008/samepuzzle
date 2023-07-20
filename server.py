c27ffc4165f94981ac0f7a75f1426abd62428583
rumya <shuriken1008@users.noreply.github.com>
2023-07-21 00:09:31 +0900

    err

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ webhook = discord_webhook.Webhook("https://discord.com/api/webhooks/113141340459
 q = queue.Queue()
 
 async def asyncio_main(cmd):
-    asyncio.ensure_future(execute_bash_command(cmd))
     asyncio.ensure_future(send_loop())
+    asyncio.ensure_future(execute_bash_command(cmd))
 
 
 async def execute_bash_command(command):
